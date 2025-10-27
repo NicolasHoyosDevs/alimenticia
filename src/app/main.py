0bcbd81cd4bfb51bec15a811dfc52eec43414efb
NicolasHoyosDevs <nhoyosg@correo.iue.edu.co>
2025-10-27 11:52:37 -0500

    last commit

--- src/app/main.py
+++ src/app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
-from .Routes.routes import router
+from fastapi.middleware.cors import CORSMiddleware
+from .Routes import router
 
 app = FastAPI()
-app.include_router(router)
-
-from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],  # O ["*"] para permitir todos (solo para desarrollo)
+    allow_origins=["*"],  # Orígenes permitidos
     allow_credentials=True,
+    # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"],  # Permite todos los headers
 )
+
+app.include_router(router)
